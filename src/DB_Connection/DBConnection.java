d4d2f1f2d21842abbc591863b255fbe688b4a633
ahmed-hani <56371326+ahmed-hani@users.noreply.github.com>
2023-01-09 17:07:18 +0200

    Update DBConnection.java

--- src/DB_Connection/DBConnection.java
+++ src/DB_Connection/DBConnection.java
@@ -21,7 +21,50 @@ public class DBConnection {
             conn = DriverManager.getConnection(  
                     "jdbc:mysql://localhost:3306/store_system","root","");
         } catch (Exception ex) {
+            System.out.println(ex);/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB_Connection;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+    //singlton design pattern
+    //for decrease performance on connection
+    //to call object just once
+    public  static DBConnection instance ;
+
+    public static DBConnection getInstance() {
+        if(instance == null){
+        instance=new DBConnection();
+        }
+        return instance;
+    }
+    
+    private DBConnection(){
+    
+    }
+   public Connection getconnection() {
+       
+      Connection conn = null;
+        try { 
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(  
+                    "jdbc:mysql://localhost:3306/store_system","root","");
+        } catch (Exception ex) {
             System.out.println(ex);
+        }
+
+   return conn;
+    }
+}
         }
 
    return conn;
